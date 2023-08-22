72de86c25479c7b0080d5b16685cfe34a8e3bbcc
unknown <randhirsingh7777777@gmail.com>
2023-08-22 23:31:53 +0530

    Update files

--- app.py
+++ app.py
@@ -1,16 +1,47 @@
 import streamlit as st
+import sys
 from dotenv import load_dotenv
 from src.logger import logging
 from src.exception import CustomException
-from src.utils import get_pdf_text, get_text_chunks, get_vector_store
+from src.utils import get_pdf_text, get_text_chunks, get_vector_store, get_conversation_chain
+from htmlTemplates import bot_template, css, user_template
 
+
+def handle_userinput(user_question):
+    logging("Generating answers for user question")
+    
+    try:
+        response = st.session_state.conversation({'question': user_question})
+        st.session_state.chat_history = response['chat_history']
+
+        for i, message in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                st.write(user_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+
+    except Exception as e:
+        raise CustomException(e, sys)
 
 def main():
     load_dotenv()
+
     st.set_page_config(page_title="Chat with your PDF's", page_icon=":PDF:")
 
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("Chat with your PDF's")
-    st.text_input("Ask anything from the uploaded PDF")
+    user_question = st.text_input("Ask anything from the uploaded PDFs")
+   
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
         st.subheader("PDF upload")
@@ -23,15 +54,13 @@ def main():
 
                 # get text chunks
                 text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks)
+                #st.write(text_chunks)
 
                 # create vector store
                 vector_store = get_vector_store(text_chunks)
 
-
-
-
-
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vector_store)
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,9 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
 def get_pdf_text(pdf_docs):
     logging.info("Reading pdf files")
@@ -33,7 +36,7 @@ def get_text_chunks(raw_texts):
             length_function=len
         )
         chunks = text_splitter.split_text(raw_texts)
-
+        logging.info("Created Text chunks")
         return chunks
 
 
@@ -45,12 +48,39 @@ def get_text_chunks(raw_texts):
 def get_vector_store(text_chunks):
     logging.info("Creting vector store")
     try:
-        #embeddings = OpenAIEmbeddings()
-        
+        embeddings = OpenAIEmbeddings()
+        #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
         vector_Store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+        logging.info("Created vector store")
         return vector_Store
 
 
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+
+def get_conversation_chain(vector_store):
+    logging.info("Creating conversation chain")
+
+    try:
+        llm = ChatOpenAI()
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+        conversation_chain = ConversationalRetrievalChain.from_llm(
+            llm=llm,
+            retriever=vector_store.as_retriever(),
+            memory=memory
+        )
+        logging("Created Conversation chain")
+        return conversation_chain
+
+    
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+
+
+
