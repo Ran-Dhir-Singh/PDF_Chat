3a9b1d81e42676c7bf936ac761ac1ef87e87eee3
unknown <randhirsingh7777777@gmail.com>
2023-08-23 12:48:14 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,15 +1,87 @@
 import streamlit as st
-import sys
 from dotenv import load_dotenv
-from src.logger import logging
-from src.exception import CustomException
-from src.utils import get_pdf_text, get_text_chunks, get_vector_store, get_conversation_chain
-from htmlTemplates import bot_template, css, user_template
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+import sys
+from logger import logging
+from exception import CustomException
+
+def get_pdf_text(pdf_docs):
+    logging.info("Reading PDF docs")
+    try:
+        text = ""
+        for pdf in pdf_docs:
+            pdf_reader = PdfReader(pdf)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        logging.info("PDF converted to text")
+        return text
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+
+def get_text_chunks(text):
+    logging.info("Converting raw text to chunks")
+    try:
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+        logging.info("Converted text to chunks")
+        return chunks
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+
+def get_vectorstore(text_chunks):
+    logging.info("Creating vector store")
+    try:
+        embeddings = OpenAIEmbeddings()
+        # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+        vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+        logging.info("Created vector store")
+        return vectorstore
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+
+def get_conversation_chain(vectorstore):
+    logging.info("Creating conversation chain")
+    try:
+        llm = ChatOpenAI()
+        # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+        memory = ConversationBufferMemory(
+            memory_key='chat_history', return_messages=True)
+        conversation_chain = ConversationalRetrievalChain.from_llm(
+            llm=llm,
+            retriever=vectorstore.as_retriever(),
+            memory=memory
+        )
+        logging.info("Created conversation chain")
+        return conversation_chain
+    
+    except Exception as e:
+        raise CustomException(e,sys)
 
 
 def handle_userinput(user_question):
-    logging("Generating answers for user question")
-    
     try:
         response = st.session_state.conversation({'question': user_question})
         st.session_state.chat_history = response['chat_history']
@@ -23,13 +95,15 @@ def handle_userinput(user_question):
                     "{{MSG}}", message.content), unsafe_allow_html=True)
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e,sys)
+    
+
 
 def main():
+    logging.info("In main function")
     load_dotenv()
-
-    st.set_page_config(page_title="Chat with your PDF's", page_icon=":PDF:")
-
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -37,33 +111,30 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with your PDF's")
-    user_question = st.text_input("Ask anything from the uploaded PDFs")
-   
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
 
     with st.sidebar:
-        st.subheader("PDF upload")
-        pdf_docs = st.file_uploader("Upload your PDF here", accept_multiple_files=True)
-        if st.button("Process PDF"):
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
             with st.spinner("Processing"):
-                # get PDF raw text
+                # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-                #st.write(raw_text)
 
-                # get text chunks
+                # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-                #st.write(text_chunks)
 
                 # create vector store
-                vector_store = get_vector_store(text_chunks)
+                vectorstore = get_vectorstore(text_chunks)
 
                 # create conversation chain
-                st.session_state.conversation = get_conversation_chain(vector_store)
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
 
 
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
